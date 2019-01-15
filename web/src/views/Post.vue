0769269aad0fbe8d4acbeaa45eb713d32bed4437
Yarn <ztytotoro@gmail.com>
2019-01-15 19:15:52 +0800

    update

---
+++ web/src/views/Post.vue
@@ -0,0 +1,12 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-class-decorator";
+
+@Component
+export default class Post extends Vue {
+
+}
+</script>
