3e604ccd8862debc28f3aa6ca951378f1da4949b
Yarn <ztytotoro@users.noreply.github.com>
2019-01-21 13:24:23 +0800

    update

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-class-decorator';
+import { Component, Vue } from "vue-class-decorator";
 
 @Component
 export default class Home extends Vue {}
