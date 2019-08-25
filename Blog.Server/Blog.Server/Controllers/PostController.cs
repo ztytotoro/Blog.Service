97655cc53d945da38e9b486acf9e92596b6ce166
Yarn <ztytotoro@outlook.com>
2019-08-26 00:01:38 +0800

    add post controller

---
+++ Blog.Server/Blog.Server/Controllers/PostController.cs
@@ -0,0 +1,18 @@
+﻿using Common.Enums;
+using DataTransfer;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Blog.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<PostDto> GetAll(Language language)
+        {
+            return new List<PostDto>();
+        }
+    }
+}
